48ef16d7cb41bea4b5f45e11b3c8c11c241e8c25
wkud <vopox07@gmail.com>
2023-04-19 21:59:06 +0200

    Add input on pick up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,32 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public Transform ItemHoldAnchor;
+    private StarterAssetsInputs _input;
 
     private void Awake()
     {
+        _input = GetComponent<StarterAssetsInputs>();
+        _input.onPickUp += OnPickUp;
+        _input.onDrop += OnDrop;
+    }
+
+    private void OnDestroy()
+    {
+        _input.onPickUp -= OnPickUp;
+        _input.onDrop -= OnDrop;
+    }
+
+    private void OnPickUp()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDrop()
+    {
+        throw new NotImplementedException();
     }
 }
